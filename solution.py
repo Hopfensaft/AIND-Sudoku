85c271bf283e7e3319754ec6b1699236d486f3d8
Hopfensaft <blaas.robert@gmail.com>
2017-09-03 21:04:37 +0100

    Added diagonal and naked twins

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 assignments = []
+DIAGONAL = True
 
 
 def cross(a, b):
@@ -10,22 +11,20 @@ rows = 'ABCDEFGHI'
 cols = '123456789'
 boxes = cross(rows, cols)
 
-values = {"A1": "", "A2": "", "A3": "", "A4": "", "A5": "", "A6": "", "A7": "", "A8": "", "A9": "",
-          "B1": "", "B2": "", "B3": "", "B4": "", "B5": "", "B6": "", "B7": "", "B8": "", "B9": "",
-          "C1": "", "C2": "", "C3": "", "C4": "", "C5": "", "C6": "", "C7": "", "C8": "", "C9": "",
-          "D1": "", "D2": "", "D3": "", "D4": "", "D5": "", "D6": "", "D7": "", "D8": "", "D9": "",
-          "E1": "", "E2": "", "E3": "", "E4": "", "E5": "", "E6": "", "E7": "", "E8": "", "E9": "",
-          "F1": "", "F2": "", "F3": "", "F4": "", "F5": "", "F6": "", "F7": "", "F8": "", "F9": "",
-          "G1": "", "G2": "", "G3": "", "G4": "", "G5": "", "G6": "", "G7": "", "G8": "", "G9": "",
-          "H1": "", "H2": "", "H3": "", "H4": "", "H5": "", "H6": "", "H7": "", "H8": "", "H9": "",
-          "I1": "", "I2": "", "I3": "", "I4": "", "I5": "", "I6": "", "I7": "", "I8": "", "I9": ""}
-
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+diagonal_units = [["A1", "B2", "C3", "D4", "E5", "F6", "G7", "H8", "I9"],
+                  ["A9", "B8", "C7", "D6", "E5", "F4", "G3", "H2", "I1"]]
+
 unitlist = row_units + column_units + square_units
+
+if DIAGONAL:
+    unitlist = unitlist + diagonal_units
+
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
+
 
 def assign_value(values, box, value):
     """
@@ -39,6 +38,7 @@ def assign_value(values, box, value):
     values[box] = value
     if len(value) == 1:
         assignments.append(values.copy())
+
     return values
 
 
@@ -67,12 +67,23 @@ def naked_twins(values):
 
             if dual_box[1] == second_dual_box[1]:
                 boxes_to_clean = boxes_to_clean + [box for box in values.keys()
-                                                   if box[1] == dual_box[1] and not (box == second_dual_box or box == dual_box)]
+                                                   if box[1] == dual_box[1]
+                                                   and not (box == second_dual_box or box == dual_box)]
+
+            for integer in range(len(square_units)):
+                if dual_box in square_units[integer] and second_dual_box in square_units[integer]:
+                    boxes_to_clean = boxes_to_clean + [box for box in square_units[integer]
+                                                       if not (box == second_dual_box or box == dual_box)]
+
+            if DIAGONAL:
+                for integer in range(len(diagonal_units)):
+                    if dual_box in diagonal_units[integer] and second_dual_box in diagonal_units[integer]:
+                        boxes_to_clean = boxes_to_clean + [box for box in diagonal_units[integer]
+                                                           if not (box == second_dual_box or box == dual_box)]
 
             for box in boxes_to_clean:
                 assign_value(values, box, values[box].replace(values[dual_box][1], ''))
                 assign_value(values, box, values[box].replace(values[dual_box][0], ''))
-                print("Tried naked twin")
 
     return values
 
@@ -105,7 +116,7 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    if values is False:
+    if values is False or None:
         print("unable to solve")
         return
     width = 1+max(len(values[s]) for s in boxes)
@@ -122,7 +133,7 @@ def eliminate(values):
     for box in solved_values:
         digit = values[box]
         for peer in peers[box]:
-            assign_value(values, peer, values[peer].replace(digit,''))
+            values = assign_value(values, peer, values[peer].replace(digit, ''))
 
     return values
 
@@ -132,12 +143,12 @@ def only_choice(values):
         for digit in '123456789':
             dplaces = [box for box in unit if digit in values[box]]
             if len(dplaces) == 1:
-                assign_value(values, dplaces[0], digit)
+                values = assign_value(values, dplaces[0], digit)
+
     return values
 
 
 def reduce_puzzle(values):
-    # solved_values = [box for box in values.keys() if len(values[box]) == 1]
     stalled = False
     while not stalled:
         solved_values_before = len(
@@ -150,6 +161,7 @@ def reduce_puzzle(values):
         stalled = solved_values_before == solved_values_after
         if len([box for box in values.keys() if len(values[box]) == 0]):
             return False
+
     return values
 
 
@@ -163,14 +175,29 @@ def search(values):
     for value in values[s]:
         new_sudoku = values.copy()
         new_sudoku[s] = value
+
         return search(new_sudoku)
 
 
 def init_grid(grid):
+
+    values = {"A1": "", "A2": "", "A3": "", "A4": "", "A5": "", "A6": "", "A7": "", "A8": "", "A9": "",
+              "B1": "", "B2": "", "B3": "", "B4": "", "B5": "", "B6": "", "B7": "", "B8": "", "B9": "",
+              "C1": "", "C2": "", "C3": "", "C4": "", "C5": "", "C6": "", "C7": "", "C8": "", "C9": "",
+              "D1": "", "D2": "", "D3": "", "D4": "", "D5": "", "D6": "", "D7": "", "D8": "", "D9": "",
+              "E1": "", "E2": "", "E3": "", "E4": "", "E5": "", "E6": "", "E7": "", "E8": "", "E9": "",
+              "F1": "", "F2": "", "F3": "", "F4": "", "F5": "", "F6": "", "F7": "", "F8": "", "F9": "",
+              "G1": "", "G2": "", "G3": "", "G4": "", "G5": "", "G6": "", "G7": "", "G8": "", "G9": "",
+              "H1": "", "H2": "", "H3": "", "H4": "", "H5": "", "H6": "", "H7": "", "H8": "", "H9": "",
+              "I1": "", "I2": "", "I3": "", "I4": "", "I5": "", "I6": "", "I7": "", "I8": "", "I9": ""}
+
     initialize_values = grid_values(grid)
 
     for val in initialize_values:
         assign_value(values, val, initialize_values[val])
+
+    return values
+
 
 def solve(grid):
     """
@@ -182,15 +209,10 @@ def solve(grid):
         The dictionary representation of the final sudoku grid.
         False if no solution exists.
     """
-    init_grid(grid)
+    values = init_grid(grid)
+    values = search(values)
 
-    search(values)
-
-    if values == False:
-        print("unsolvable!")
-        return False ## Failed earlier
-    if all(len(values[s]) == 1 for s in boxes):
-        return values ## Solved!
+    return values
 
 
 if __name__ == '__main__':
